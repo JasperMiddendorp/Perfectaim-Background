ae03597e79a22eddfbb92972dcc9f126db2cee3b
Jasperkiller1 <36958960+Jasperkiller1@users.noreply.github.com>
2018-12-30 23:33:55 +0100

    Update Perfectaim_Background.user.js

--- Perfectaim_Background.user.js
+++ Perfectaim_Background.user.js
@@ -11,56 +11,56 @@
 // ==/UserScript==
 var $ = window.jQuery;
 
-$(function() {
+$(function() { // this removes the 2 scrolling banners from the member page
     $('#cs').remove();
     $('#csads').remove();
 });
 
 
 GM_addStyle ( `
-body {
-background-color: #292c31d1 !important;
+body { // make the body transparent
+	background-color: #292c31d1 !important;
 }
 
-#header .container{
-padding: 0px !important;
+#header .container{ // to make the header look cleaner
+	padding: 0px !important;
 }
 
-.navbar{
-background-color: #292c31d1 !important;
+.navbar{ // navigation bar
+	background-color: #292c31d1 !important;
 }
 
-.card{
-background-color: #22252a00 !important;
+.card{ // all the cards on member screen
+	background-color: #22252a00 !important;
 }
 
-#header{
-background: #292c31 !important;
+#header{ // header
+	background: #292c31 !important;
 }
 
-.forum-post{
-background-color: #22252a00 !important;
-border-style: solid !important;
-border-width: 2px 10px 2px 10px !important;
-border-color: #00000052;
+.forum-post{ // forum posts
+	background-color: #22252a00 !important;
+	border-style: solid !important;
+	border-width: 2px 10px 2px 10px !important;
+	border-color: #00000052;
 }
 
-.container {
-padding: 20px !important;
-background: #292c31d1 !important;
+.container { // all text-containing boxes get a padding
+	padding: 20px !important;
+	background: #292c31d1 !important;
 }
 
-.fixed-header {
-background-color: #292c31d1 !important;
-        background: url('https://i.ibb.co/BTnRKtw/PABackground2.png') no-repeat center center fixed !important;
-        -webkit-background-size: auto !important;
-        -moz-background-size: auto !important;
-        -o-background-size: auto !important;
-        background-size: auto !important;
+.fixed-header { // background replacer
+	background-color: #292c31d1 !important;
+    background: url('https://i.ibb.co/BTnRKtw/PABackground2.png') no-repeat center center fixed !important;
+    -webkit-background-size: auto !important;
+    -moz-background-size: auto !important;
+    -o-background-size: auto !important;
+    background-size: auto !important;
 
-        -webkit-background-repeat: repeat !important;
-        -moz-background-repeat: repeat !important;
-        -o-background-repeat: repeat !important;
-        background-repeat: repeat !important;
+    -webkit-background-repeat: repeat !important;
+    -moz-background-repeat: repeat !important;
+    -o-background-repeat: repeat !important;
+    background-repeat: repeat !important;
     }
 ` );
